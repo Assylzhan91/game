50cc51b55c7e2e5e49e095d403e91bf28d12c70e
Асылжан Баймуратов <assylzhan.baimuratov@dmms.kz>
2025-10-12 18:43:31 +0500

    Add launcher & root package

---
+++ app/src/com/java/assyl/tictactoe/Launcher.java
@@ -0,0 +1,10 @@
+package com.java.assyl.tictactoe;
+
+/**
+ * @Assylzhan Baimuratov
+ **/
+public final class Launcher {
+    public  static void main(final String[] args) {
+        System.out.println("Welcome to TicTacToe!");
+    }
+}
