6cb8c4face20f49f8bacce1141ca3beb95532f8b
Асылжан Баймуратов <assylzhan.baimuratov@dmms.kz>
2025-12-16 16:09:44 +0500

    lesson 015 module-17

--- app/src/com/java/assyl/tictactoe/model/GameTable.java
+++ app/src/com/java/assyl/tictactoe/model/GameTable.java
@@ -4,4 +4,21 @@ package com.java.assyl.tictactoe.model;
  * @Assylzhan Baimuratov
  **/
 public class GameTable {
+    private final char[][] table = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+    };
+
+    public boolean isEmpty(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()] == ' ';
+    }
+
+    public char getSign(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()];
+    }
+
+    public void setSign(final Cell cell, final char sign) {
+        table[cell.getRow()][cell.getCol()] = sign;
+    }
 }
