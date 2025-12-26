768e5094064a7bc4c260aec1d8b8fda28dbbc465
Асылжан Баймуратов <assylzhan.baimuratov@dmms.kz>
2025-12-26 14:55:33 +0500

    lesson 019 module-17

--- app/src/com/java/assyl/tictactoe/component/DataPrinter.java
+++ app/src/com/java/assyl/tictactoe/component/DataPrinter.java
@@ -1,5 +1,6 @@
 package com.java.assyl.tictactoe.component;
 
+import com.java.assyl.tictactoe.model.Cell;
 import com.java.assyl.tictactoe.model.GameTable;
 
 /**
@@ -9,11 +10,25 @@ public class DataPrinter implements Printer {
 
     @Override
     public void printMappingTable() {
-        System.out.println("printMappingTable");
+        System.out.println("-------------");
+        System.out.println("| 7 | 8 | 9 |");
+        System.out.println("-------------");
+        System.out.println("| 4 | 5 | 6 |");
+        System.out.println("-------------");
+        System.out.println("| 1 | 2 | 3 |");
+        System.out.println("-------------");
     }
 
     public void printGameTable(final GameTable gameTable) {
 
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable.getSign(new Cell(i, j)) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 }
 
