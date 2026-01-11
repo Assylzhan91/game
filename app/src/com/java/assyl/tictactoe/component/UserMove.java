5b9aebc790c079d165da5bcfa7406b99cac8ea2e
Assylzhan_Baimuratov <asylzhan.baimuratov@gmail.com>
2026-01-11 19:50:35 +0500

    lesson 26 module-17

--- app/src/com/java/assyl/tictactoe/component/UserMove.java
+++ app/src/com/java/assyl/tictactoe/component/UserMove.java
@@ -4,20 +4,49 @@ import com.java.assyl.tictactoe.model.Cell;
 import com.java.assyl.tictactoe.model.GameTable;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * @Assylzhan Baimuratov
  **/
 public class UserMove {
+    private final char[][] mapping = {
+            {'7', '8', '9'},
+            {'4', '5', '6'},
+            {'1', '2', '3'},
+    };
+
     public void make(final GameTable gameTable) {
-        final Random random = new Random();
         while (true) {
-            final int row = random.nextInt(3);
-            final int col = random.nextInt(3);
-            final Cell randomCell = new Cell(row, col);
-            if (gameTable.isEmpty(randomCell)) {
-                gameTable.setSign(randomCell, 'X');
+            final Cell cell = getUserInput();
+            if (gameTable.isEmpty(cell)) {
+                gameTable.setSign(cell, 'X');
                 return;
+            } else {
+                System.out.println("Can't make a move, because the cell is not free! Try again!");
+            }
+        }
+    }
+
+    private Cell getUserInput() {
+        while (true) {
+            System.out.println("Please type number between 1 and 9: ");
+            final String userInput = new Scanner(System.in).nextLine();
+
+            if (userInput.length() == 1) {
+                final char ch = userInput.charAt(0);
+                System.out.println(ch);
+                if (ch >= '1' && ch <= '9') {
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            if (mapping[i][j] == ch) {
+                                return new Cell(i, j);
+                            }
+                        }
+                    }
+                } else {
+                    System.out.println("Invalid input. Please enter a number between 1 and 9.");
+                }
             }
         }
     }
