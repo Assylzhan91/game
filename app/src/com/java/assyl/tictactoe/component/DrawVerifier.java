1fecac2a490efe078450588412e7424e9f406d91
Assylzhan_Baimuratov <asylzhan.baimuratov@gmail.com>
2026-01-15 21:53:31 +0500

    lesson 29 module-17

--- app/src/com/java/assyl/tictactoe/component/DrawVerifier.java
+++ app/src/com/java/assyl/tictactoe/component/DrawVerifier.java
@@ -1,12 +1,22 @@
 package com.java.assyl.tictactoe.component;
 
+import com.java.assyl.tictactoe.model.Cell;
 import com.java.assyl.tictactoe.model.GameTable;
 
 /**
  * @Assylzhan Baimuratov
  **/
 public class DrawVerifier {
+
     public boolean isDraw(final GameTable gameTable) {
-        return false;
+        for (int i = 0; i < 3; i++) {
+             for (int j = 0; j < 3; j++) {
+                Cell cell = new Cell(i, j);
+                if (gameTable.isEmpty(cell)) {
+                    return false;
+                }
+             }
+        }
+        return true;
     }
 }
